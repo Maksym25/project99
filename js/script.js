8e591e9cab51ad8f933a2fb94513dc7aa3776795
Maksym25 <mikisimi25@gmail.com>
2020-06-14 17:30:47 +0200

    add object personalMovieDB to script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 "use strict";
 
 const numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
-console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
