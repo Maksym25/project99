8c9d3085c3e4221e41653004dcaa281a973071a0
Maksym25 <mikisimi25@gmail.com>
2020-06-14 17:44:00 +0200

    add answers for movies

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
+const numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
 
 const personalMovieDB = {
     count : numberOfFilms,
@@ -9,3 +9,13 @@ const personalMovieDB = {
     genres : [],
     privat : false
 };
+
+const   a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколька оцените его?", ""),
+        c = prompt("Один из последних просмотренных фильмов?", ""),
+        d = prompt("На сколька оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
